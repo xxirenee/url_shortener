4c3ebd27025fdf5e0ebad1542428d53c7654527e
xxirenee <charice0709@gmail.com>
2022-10-23 00:19:53 +0800

    modify express routes

--- app.js
+++ app.js
@@ -5,24 +5,15 @@ const exphbs = require('express-handlebars')
 require('./config/mongoose')
 const app = express()
 const port = 3000
+const routes = require('./routes')
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
 
-const Url = require('./models/url')
-
-app.get('/', (req, res) => {
-  Url.find()
-    .lean()
-    .then(urls => res.render('index', { urls }))
-    .catch(error => console.error(error))
-})
 
 // start and listen on the Express server
 app.listen(port, () => {
